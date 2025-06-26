e78c91e6fd84578bd9da6ee04d2a83ba8c665035
Benjemenjamin <bennkiech@gmail.com>
2025-06-26 12:11:58 +0200

    blocks

---
+++ first code/Blocks/blocks.js
@@ -0,0 +1,23 @@
+"use strict";
+//let x1: number = 2;
+//let x2: number = 2;
+//let x: number = x1 + x2;
+//console.log(x);
+//if (x == 4) {
+//console.log("x is 4");
+//} else {
+//  console.log("x is not 4");
+//}
+let x = 2;
+switch (x) {
+    case 1:
+        console.log("Hello");
+        break;
+    case 2:
+        console.log("goodbe");
+        break;
+    default:
+        console.log("Hug");
+        break;
+}
+console.log("Done");
---
+++ first code/test.ts
@@ -0,0 +1,4 @@
+console.log("hello");
+let v: number= 1;
+v = v + 1;
+console.log(v);
