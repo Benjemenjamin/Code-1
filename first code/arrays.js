b4a2686b7db2a141bd65bdc56d19a45e4caf7814
Benjemenjamin <bennkiech@gmail.com>
2025-07-02 09:33:16 +0200

    dice game startet

--- first code/arrays.js
+++ first code/arrays.js
@@ -1,10 +1,5 @@
 "use strict";
-let b = 100000000000000000;
-let a = 50000000000000000;
-for (let i = b; i > 1; i /= 2) {
-    if (i == 3)
-        continue;
-    if (i == a)
-        break;
-    console.log(i);
+let o = [4, 7, 1];
+for (let value in o) {
+    console.log(o);
 }
--- first code/arrays.ts
+++ first code/arrays.ts
@@ -1,12 +1,4 @@
-let b: number=100000000000000000
-let a: number=50000000000000000
-
-
-for (let i: number = b; i > 1; i/=2) {
-  if (i == 3)
-    continue;
-  if (i == a)
-    break;
-  console.log(i);
+let o = [4,7,1];
+for (let value in o) {
+  console.log(o);
 }
-
